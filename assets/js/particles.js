ba18461d5e02746ce470ed16ad15d632f7125efa
Abhishek Kumar <38509489+sudoabhi@users.noreply.github.com>
2020-10-14 19:42:09 +0530

    Update particles.js

--- assets/js/particles.js
+++ assets/js/particles.js
@@ -5,11 +5,11 @@ particlesJS("particles-js", {
       "value": 80,
       "density": {
         "enable": true,
-        "value_area": 700 } },
+        "value_area": 1200 } },
 
 
     "color": {
-      "value": "#ffffff" },
+      "value": "#000000" },
 
     "shape": {
       "type": "circle",
@@ -26,17 +26,17 @@ particlesJS("particles-js", {
       "random": false,
       "anim": {
         "enable": false,
-        "speed": 0.1,
+        "speed": 1,
         "opacity_min": 0.1,
         "sync": false } },
 
 
     "size": {
-      "value": 3,
+      "value": 5,
       "random": true,
       "anim": {
         "enable": false,
-        "speed": 10,
+        "speed": 40,
         "size_min": 0.1,
         "sync": false } },
 
@@ -44,13 +44,13 @@ particlesJS("particles-js", {
     "line_linked": {
       "enable": true,
       "distance": 150,
-      "color": "#ffffff",
+      "color": "#000000",
       "opacity": 0.4,
       "width": 1 },
 
     "move": {
       "enable": true,
-      "speed": 2,
+      "speed": 3,
       "direction": "none",
       "random": false,
       "straight": false,
